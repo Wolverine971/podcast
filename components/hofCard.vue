8f9aa31e725e20bffb1efabe5429dcca69e46726
David Wayne <djwayne35@gmail.com>
2021-04-24 22:01:16 -0400

    first commit

---
+++ components/hofCard.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-col
+    cols="12"
+    :md="size === 2 ? 6 : size === 3 ? 4 : 2"
+  >
+    <base-card
+      :height="value.prominent ? 450 : 350"
+      color="grey lighten-1"
+      dark
+      href="#!"
+    >
+      <v-img
+        :src="`https://img.youtube.com/vi/${value.src}/maxresdefault.jpg`"
+        height="100%"
+        gradient="rgba(0, 0, 0, .42), rgba(0, 0, 0, .42)"
+      >
+        <v-row
+          v-if="!value.prominent"
+          class="fill-height text-right ma-0"
+        >
+          <v-col cols="12">
+            <v-chip
+              label
+              class="mx-0 mb-2 text-uppercase"
+              color="grey darken-3"
+              text-color="white"
+              small
+              @click.stop=""
+            >
+              {{ value.category }}
+            </v-chip>
+
+            <h3 class="title font-weight-bold mb-2 twhite">
+              {{ value.title }}
+            </h3>
+
+            <div class="caption twhite">
+              {{ value.author }}<br>
+            </div>
+          </v-col>
+
+          <v-col align-self="end">
+              <v-btn small outlined link :href="`https://www.youtube.com/watch?v=${value.src}`" class="text-uppercase ma-0 twhite">Go To Video</v-btn>
+          </v-col>
+        </v-row>
+      </v-img>
+    </base-card>
+  </v-col>
+</template>
+
+<script>
+  export default {
+    name: 'hofCard',
+
+    props: {
+      size: {
+        type: Number,
+        required: true,
+      },
+      value: {
+        type: Object,
+        default: () => ({}),
+      },
+    },
+  }
+</script>
+
+<style>
+.v-image__image {
+  transition: .3s linear;
+}
+.twhite {
+    color: whitesmoke !important;
+}
+.bwhite{
+    background-color: whitesmoke;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-text>
+          <div class="text-center">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/RqTBCe5VuhE"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <hr class="my-3" />
+          <p>
+            You dont know until you try
+          </p>
+          <div class="text-xs-right">
+            <em><small>&mdash; DJ Wayne</small></em>
+          </div>
+          <hr class="my-3" />
+          <p>
+            There is a podcast that is gonna be happening soon.
+          </p>
+          <p>
+            The topics we are going to be covering are:
+          </p>
+          <ul>
+            <li>
+              Abortion
+            </li>
+            <li>
+              TBD
+
+            </li>
+            <li>
+            </li>
+          </ul>
+        </v-card-text>
+      </v-card>
+      <v-card>
+        <v-card-text>
+          <div class="text-center">
+            <base-timer />
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import BaseTimer from '~/components/baseTimer.vue'
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo,
+    BaseTimer,
+  },
+  data: () => ({
+    links: ['Dashboard', 'Messages', 'Profile', 'Updates'],
+  }),
+}
+</script>
