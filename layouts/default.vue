942d261106f4c044d419ce689dd0a9feafdd609b
david.t.wayne <david.t.wayne.ctr@usmc.mil>
2021-10-18 01:29:19 -0400

    added nick

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
   <v-app dark :class="$route.path.includes('about') ? 'city' : 'djFace'">
     <v-app-bar app color="white" flat>
-      <v-tabs centered class="ml-n9" color="grey darken-1">
+      <div v-if="$axios.defaults.baseURL.includes('nickanddj')"></div>
+      <v-tabs centered class="ml-n9" color="grey darken-1" v-else>
         <v-tab v-for="(link, i) in links" :key="i" :to="link.route">
           {{ link.name }}
         </v-tab>
@@ -12,17 +13,7 @@
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
+    <v-footer :absolute="true" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -32,24 +23,7 @@
 export default {
   data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
+      
       title: 'Vuetify.js',
       links: [
         {
--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,22 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <v-card>
+        <v-card-title v-if="$axios.defaults.baseURL.includes('nickanddj')">
+          NICK AND DJ TALK ABOUT STUFF DOT COM!
+        </v-card-title>
         <v-card-text>
-          <div class="text-center">
+          <div class="text-center" v-if="$axios.defaults.baseURL.includes('nickanddj')">
+            <iframe
+              width="100%"
+              height="700vw"
+              class="iframe-placeholder"
+              src="https://www.youtube.com/embed/ccS5crpR9dQ"
+              title="Pod Pod"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            ></iframe>
+            </div>
+          <div class="text-center" v-else>
             <iframe
               width="100%"
               height="700vw"
@@ -15,9 +29,14 @@
             ></iframe>
           </div>
           <hr class="my-3" />
-          <p>You dont know until you try</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; DJ Wayne</small></em>
+          <div v-if="$axios.defaults.baseURL.includes('nickanddj')">
+            <q>Up up and away!</q>
+              <cite>&mdash; Nick</cite>
+
+          </div>
+          <div v-else>
+            <q>You dont know until you try</q>
+              <cite>&mdash; DJ</cite>
           </div>
           <hr class="my-3" />
           <h1>Podcast</h1>
